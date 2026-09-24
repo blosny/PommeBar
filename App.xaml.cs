ba0b40031869e7ad3504dab55279ff71677a13e8
Taha Buğra Çiçek <tahabugracicek.dev@gmail.com>
2026-09-25 01:45:38 +0300

    fix: add global unhandled exception logging to App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,29 @@
-using System.Configuration;
-using System.Data;
+using System;
+using System.IO;
 using System.Windows;
 
 namespace PommeBar;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-}
+    public App()
+    {
+        AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+        {
+            try
+            {
+                File.AppendAllText(@"C:\projects\pomme-bar\crash.log", $"[AppDomain Unhandled] {DateTime.Now}: {e.ExceptionObject}\n");
+            }
+            catch { }
+        };
 
+        this.DispatcherUnhandledException += (s, e) =>
+        {
+            try
+            {
+                File.AppendAllText(@"C:\projects\pomme-bar\crash.log", $"[Dispatcher Unhandled] {DateTime.Now}: {e.Exception}\n");
+            }
+            catch { }
+        };
+    }
+}
