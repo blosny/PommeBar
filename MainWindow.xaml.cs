cffd6b88c4a01dffa03898bc22fa5e528ff6bfd3
Taha Buğra Çiçek <tahabugracicek.dev@gmail.com>
2026-09-24 15:01:37 +0300

    Add modern Mica UI design, WPF-UI package, and gitignore

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,22 @@
-﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace PommeBar;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : FluentWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            this.DragMove();
+        }
     }
 }
